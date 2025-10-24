fe7eadfe3126ea6455177fce623ce7c46ade019c
FatahulFahmi <fatahulfahmi1234@gmail.com>
2025-10-24 17:58:26 +0700

    mengubah app.js menjadi Argumen Parsing

--- Jobsheet 3 - Node.Js Module System/buku-catatan/app.js
+++ Jobsheet 3 - Node.Js Module System/buku-catatan/app.js
@@ -1,13 +1,59 @@
-const fs = require("fs");
-// fs.writeFileSync("catatan.txt", "Nama Saya Fatahul Fahmi");
-fs.appendFileSync("catatan.txt", " Saya tinggal di Padang");
+// Import modul yang dibutuhkan
+const { hideBin } = require("yargs/helpers");
+const yargs = require("yargs/yargs")(hideBin(process.argv));
+const catatan = require("./catatan.js");
 
-// const catatan = require("./catatan.js");
-// const pesan = catatan();
-// console.log(pesan);
+// Kustomisasi versi yargs
+yargs.version("10.1.0");
 
-const validator = require("validator");
-const ambilCatatan = require("./catatan.js");
-const pesan = ambilCatatan();
-console.log(pesan);
-console.log(validator.isURL("https://fahmi.com"));
+// Command: tambah catatan
+yargs.command({
+  command: "tambah",
+  describe: "tambah sebuah catatan baru",
+  builder: {
+    judul: {
+      describe: "Judul catatan",
+      demandOption: true,
+      type: "string",
+    },
+    isi: {
+      describe: "Isi catatan",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Judul: " + argv.judul);
+    console.log("Isi: " + argv.isi);
+  },
+});
+
+// Command: hapus catatan
+yargs.command({
+  command: "hapus",
+  describe: "hapus catatan",
+  handler: function () {
+    console.log("Catatan berhasil dihapus");
+  },
+});
+
+// Command: tampilkan semua catatan
+yargs.command({
+  command: "list",
+  describe: "menampilkan daftar semua catatan",
+  handler: function () {
+    console.log("Menampilkan semua catatan...");
+  },
+});
+
+// Command: baca catatan
+yargs.command({
+  command: "baca",
+  describe: "membaca sebuah catatan",
+  handler: function () {
+    console.log("Membaca catatan...");
+  },
+});
+
+// Jalankan parser Yargs
+yargs.parse();
